ee4e20e59a1bce9941047c817b8f94798464af8c
green1052 <34261355+green1052@users.noreply.github.com>
2023-02-10 03:17:13 +0900

    1

---
+++ build.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const {EOL} = require("os");
+
+const REGEX = fs.readFileSync("./hosts.txt", "utf8")
+    .split(EOL)
+    .filter(host => host !== "" && !host.startsWith("#"))
+    .join("|")
+    .replaceAll(".", "\\.");
+
+fs.writeFileSync(`filters-share/search.txt`, `google.*#?#div[role="main"] div#search div[data-async-context] div.g[data-hveid]:-abp-contains(/${REGEX}/)`, "utf8");
